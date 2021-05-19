c48c6b6c530344c2001216abc1ae353df5178bb8
Jason C <jayingmantis.555@gmail.com>
2021-05-19 13:43:16 -0500

    part 6 complete

--- script.js
+++ script.js
@@ -13,27 +13,50 @@ document.addEventListener("DOMContentLoaded", function(event) {
     document.querySelector('#quote-of-the-day').textContent = `"${quotes[Math.floor(Math.random() * quotes.length)]}"`;
   };
   randomQuote();
-  
-  // Do all of your work inside the document.addEventListener  
+
+
+  // Do all of your work inside the document.addEventListener
 
   // Part 1
-
-
+const shorterTitle = function() {
+  document.querySelector('#main-title').textContent = 'Welcome to the Dom';
+}
+shorterTitle();
   // Part 2
-
-
+const bodyBackground = function() {
+  let body = document.querySelector('body');
+  body.style.backgroundColor = '#bf897e';
+}
+bodyBackground();
   // Part 3
 
-
+const listChange = function() {
+  let list = document.getElementById('favorite-things');
+  list.removeChild(list.lastElementChild);
+}
+listChange();
   // Part 4
-
-
+const specialFontChange = function () {
+  let specialFont = document.querySelectorAll('.special-title');
+  specialFont.forEach(function(i) {
+    i.style.fontSize = '2rem';
+  });
+}
+specialFontChange();
   // Part 5
-
-
+const removeChicago = function(){
+  let removeRace = document.getElementById('past-races');
+  removeRace.removeChild(removeRace.childNodes[7]);
+}
+removeChicago();
   // Part 6
-
-
+const addPastRace = function() {
+  let ul = document.getElementById('past-races');
+  let li = document.createElement('li');
+  li.appendChild(document.createTextNode('Burlington'));
+  ul.appendChild(li);
+}
+addPastRace();
   // Part 7
 
 
@@ -45,4 +68,4 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
 
 
-});
+ });
